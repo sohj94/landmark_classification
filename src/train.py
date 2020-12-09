208ccc3a108eb3b0378dd388011e0659553d2e3d
Hyungjun So <sohj94@gmail.com>
2020-12-10 01:46:20 +0900

    GoogLeNet 추가 및 학습 이어하기 로직 추가

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ import torch
 import argparse
 from torch.utils.data import DataLoader
 from torch import nn, optim
+from torch.utils.tensorboard import SummaryWriter
 
 from dataset_builder import TrainDataset, TestDataset
 from trainer import Trainer
@@ -12,6 +13,7 @@ from trainer import Trainer
 # import model
 from model.three_layer_conv_net import three_layer_conv_net
 from model.alexnet import alexnet
+from model.googlenet import GoogLeNet
 
 parser = argparse.ArgumentParser()
 
@@ -24,7 +26,7 @@ parser.add_argument('--test_csv_dir', dest='test_csv_dir', default="../data/publ
 parser.add_argument('--test_csv_exist_dir', dest='test_csv_exist_dir', default="../data/public/sample_submission_exist.csv")
 
 parser.add_argument('--test_csv_submission_dir', dest='test_csv_submission_dir', default="../data/public/my_submission.csv")
-parser.add_argument('--model_dir', dest='model_dir', default="../data/ckpt/")
+parser.add_argument('--model_dir', dest='model_dir', default="../data/ckpt/temp/")
 
 parser.add_argument('--image_size', dest='image_size', type=int, default=256)
 parser.add_argument('--epochs', dest='epochs', type=int, default=100)
@@ -32,7 +34,8 @@ parser.add_argument('--learning_rate', dest='learning_rate', type=float, default
 parser.add_argument('--wd', dest='wd', type=float, default=1e-5)
 parser.add_argument('--batch_size', dest='batch_size', type=int, default=64)
 
-parser.add_argument('--train', dest='train', type=bool, default=True)
+parser.add_argument('--train', dest='train', action='store_false', default=True)
+parser.add_argument('--continue_train', dest='continue_train', action='store_true', default=False)
 parser.add_argument('--load_epoch', dest='load_epoch', type=int, default=29)
 
 args = parser.parse_args()
@@ -54,13 +57,17 @@ def collate_fn_test(batch) :
 
     return torch.tensor(image).float().cuda(), label
 
+# writer = SummaryWriter('runs/alexnet')
+
 # Dataset, Dataloader 정의
 train_dataset = TrainDataset(args)
 test_dataset = TestDataset(args)
 train_data = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
 test_data = DataLoader(test_dataset, batch_size=1, shuffle=False, collate_fn=collate_fn_test)
 
-model = alexnet()
+# model = three_layer_conv_net()
+# model = alexnet()
+model = GoogLeNet(num_classes=1049, aux_logits=False, init_weights=True)
 model.cuda()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.wd)
@@ -70,7 +77,13 @@ trainer = Trainer(model, criterion, optimizer, args)
 
 #train
 if args.train:
-	trainer.fit(train_data)
+	if args.continue_train :
+		last_epoch = int(os.listdir(args.model_dir)[-1].split('epoch_')[1][:3])
+		model.load_state_dict(torch.load(args.model_dir + "epoch_{0:03}.pth".format(last_epoch)))
+		# 그 다음 epoch부터 학습 시작
+		trainer.fit(train_data, last_epoch+1)
+	else :
+		trainer.fit(train_data)
 else:
 	model.load_state_dict(torch.load(args.model_dir + "epoch_{0:03}.pth".format(args.load_epoch)))
 
--- src/trainer.py
+++ src/trainer.py
@@ -11,35 +11,35 @@ class Trainer:
 		self.optimizer = optimizer
 		self.args = args
 
-	def fit(self, train_data):
+	def fit(self, train_data, last_epoch=0):
 		self.model.train()
 		for epoch in range(self.args.epochs):
 			epoch_loss = 0.
 			for iter, (image, label) in enumerate(train_data) :
-				tmp = time.time()
+				# tmp = time.time()
 				pred = self.model(image)
 				loss = self.criterion(input=pred, target=label)
-
 				self.optimizer.zero_grad()
 				loss.backward()
 				self.optimizer.step()
 				epoch_loss += loss.detach().item()
-				print("Ran in {} seconds".format(time.time() - tmp))
-				print('epoch : {0} step : [{1}/{2}] loss : {3}'.format(epoch, iter, len(train_data), loss.detach().item()))
+				# print("Ran in {} seconds".format(time.time() - tmp))
+				print('epoch : {0} step : [{1}/{2}] loss : {3}'.format(epoch+last_epoch, iter, len(train_data), loss.detach().item()))
 			epoch_loss /= len(train_data)
 			print('\nepoch : {0} epoch loss : {1}\n'.format(epoch, epoch_loss))
 
-			torch.save(self.model.state_dict(), args.model_dir + "epoch_{0:03}.pth".format(epoch))
+			torch.save(self.model.state_dict(), self.args.model_dir + "epoch_{0:03}.pth".format(epoch))
 
 	def test(self, test_data):
-		model.eval()
-		submission = pd.read_csv(args.test_csv_dir)
+		self.model.eval()
+		submission = pd.read_csv(self.args.test_csv_dir)
 		for iter, (image, label) in enumerate(test_data):
-			pred = model(image)
-			pred = nn.Softmax(dim=1)(pred)
+			pred = self.model(image)
+			pred = nn.Softmax(dim=0)(pred)
 			pred = pred.detach().cpu().numpy()
-			landmark_id = np.argmax(pred, axis=1)
-			confidence = pred[0,landmark_id]
+			landmark_id = np.argmax(pred)
+			confidence = pred[landmark_id]
+			print(confidence, landmark_id)
 			submission.loc[iter, 'landmark_id'] = landmark_id
 			submission.loc[iter, 'conf'] = confidence
-		submission.to_csv(args.test_csv_submission_dir, index=False)
+		submission.to_csv(self.args.test_csv_submission_dir, index=False)
